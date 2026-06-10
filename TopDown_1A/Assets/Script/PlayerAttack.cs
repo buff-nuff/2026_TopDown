bbabd2aa238d37f311f5e6720aedce24422cff0b
buff-nuff <xogkswjd@g.shingu.ac.kr>
2026-06-10 20:56:34 +0900

    적 애니메이션 작업 / 적 데이터 입력

--- TopDown_1A/Assets/Script/PlayerAttack.cs
+++ TopDown_1A/Assets/Script/PlayerAttack.cs
@@ -36,7 +36,7 @@ public class PlayerAttack : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.F))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             ShootTargeted();
         }
@@ -49,7 +49,7 @@ public class PlayerAttack : MonoBehaviour
         // ⚡ [최적화 2] Physics2D.OverlapCircleNonAlloc 사용
         // OverlapCircleAll은 호출할 때마다 새로운 배열을 힙 메모리에 생성하여 순간적인 끊김(렉)을 유발합니다.
         // NonAlloc 방식을 쓰면 기존에 만들어 둔 targetResults 배열에 알맹이만 채워 넣으므로 렉이 전혀 없습니다.
-        int targetCount = Physics2D.OverlapCircleNonAlloc(transform.position, targetSearchRadius, targetResults, enemyLayer);
+        Collider2D[] allEnemyTargets = Physics2D.OverlapCircleAll(transform.position, targetSearchRadius, enemyLayer);
         
         Transform closestEnemy = null;
         float closestDistanceSqr = Mathf.Infinity; // 제곱 거리를 비교할 것이므로 초기값은 무한대
@@ -57,21 +57,21 @@ public class PlayerAttack : MonoBehaviour
         // ⚡ [최적화 3] Vector2.Distance 대치 ➔ sqrMagnitude 사용
         // 컴퓨터는 루트(√) 연산을 할 때 엄청난 연산 부하를 느낍니다. Distance 함수 내부에는 루트 연산이 들어있습니다.
         // 거리의 제곱 비중을 비교하는 sqrMagnitude를 사용하면 루트 연산이 완전히 생략되어 연산 속도가 수십 배 빨라집니다.
-        for (int i = 0; i < targetCount; i++)
+        foreach (Collider2D col in allEnemyTargets)
         {
             // 가끔 배열 구멍에 비어있는 값이 들어오는 것을 방지
-            if (targetResults[i] == null) continue;
+            if (col == null) continue;
 
             // GetComponent 횟수를 줄이기 위해 Collider가 살아있는지 기본 검사 후 거리 비교
-            float distanceSqr = (targetResults[i].transform.position - transform.position).sqrMagnitude;
+            float distanceSqr = (col.transform.position - transform.position).sqrMagnitude;
             
             if (distanceSqr < closestDistanceSqr)
             {
                 // 진짜 적인지 컴포넌트 최종 확인
-                if (targetResults[i].TryGetComponent<EnemyAI>(out var enemy))
+                if (col.TryGetComponent<EnemyAI>(out var enemy))
                 {
                     closestDistanceSqr = distanceSqr;
-                    closestEnemy = targetResults[i].transform;
+                    closestEnemy = col.transform;
                 }
             }
         }
